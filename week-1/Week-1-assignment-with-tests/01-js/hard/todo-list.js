22b070adda41723d5fea8e6cfd6fb8aae3c8ec49
litheshshetty <litheshshettytech@gmail.com>
2024-01-28 01:17:03 +0530

    Week 1 01-js completed

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -13,6 +13,43 @@
 
 class Todo {
 
+  constructor() {
+    this.todos = [];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if(indexOfTodo < 0 || indexOfTodo >= this.todos.length) {
+      return;
+    }
+    this.todos.splice(indexOfTodo, 1);
+  }
+
+  update(indexOfTodo, updatedTodo) {
+    if(indexOfTodo < 0 || indexOfTodo >= this.todos.length) {
+      return;
+    }
+    this.todos[indexOfTodo] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(indexOfTodo) {
+    if(indexOfTodo < 0 || indexOfTodo >= this.todos.length) {
+      return null;
+    }
+    return this.todos[indexOfTodo];
+  }
+
+  clear() {
+    this.todos.splice(0)
+  }
+
 }
 
 module.exports = Todo;
