23ac1208c961a588651cff8b40a8f4b729308008
shettylithesh <shettylithesh6@gmail.com>
2024-09-22 03:49:05 +0530

    async-js-code-updated

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -4,15 +4,21 @@
 
 function wait(n) {
     return new Promise((resolve, reject)=>{
+        console.log("Async code inside promise object getting started") //1
         setTimeout(()=>{
             resolve();
         }, n * 1000);
+        console.log("Main thread was here first and was not blocked by setTimeout"); //2
     })
 }
 
 let n = 5;
 let PromiseObj = wait(n);
 
+console.log("Async code has started in the background since promise obj is created, it doesn't care about promise resolution"); //3
+
 PromiseObj.then(()=>{
-    console.log(`Promise resolved after ${n} seconds`)
+    console.log(`Promise resolved after ${n} seconds`) //5 .then() is async its waiting for promiseObj to get resolved, once its resolved only then it adds the callback to the queue 
 });
+
+console.log("Main thread was finally here without getting blocked"); //4
--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/4-promise-chain.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/4-promise-chain.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
  * Write a function that sequentially calls all 3 of these functions in order.
  * Print out the time it takes to complete the entire operation.
